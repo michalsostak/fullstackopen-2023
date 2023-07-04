ec098be121fef39ecae491fd99e9e96419369435
Michal Sostak <michal.sostak@gmail.com>
2023-07-04 10:47:36 +0200

    feat: part 2 countries 2.18-2.20 exercises

---
+++ part2/countries/src/services/weather.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+const apiKey = process.env.REACT_APP_API_KEY
+
+const getCurrentTemp = (lat, lon) => {
+    const request = axios.get(`${baseUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+    console.log(request)
+    return request.then(response => response.data)
+}
+
+export default { getCurrentTemp }
