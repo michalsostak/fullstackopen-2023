ea85898647737badf98293f42a1beb4799c55b07
Michal Sostak <michal.sostak@gmail.com>
2023-07-06 22:33:40 +0200

    feat: mongo for phonebook exercise 3.12

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://michalsostak:${password}@cluster0.f1vgnnl.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  console.log("phonebook:")
+  Person.find().then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+  return
+}
+
+if (rocess.argv.length == 4) {
+  console.log("Name or number for a new person is missing")
+  return
+}
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+person.save().then(result => {
+  console.log('new person saved!')
+  mongoose.connection.close()
+})
