325a445e17022d77c4432a127c3fe8be741ab9f4
Michal Sostak <michal.sostak@gmail.com>
2023-07-09 00:00:20 +0200

    feat: backend with validation and eslint before fix of linting warnings

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -18,8 +18,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
-
     next(error)
 }
 
@@ -74,20 +75,26 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
     const note = {
         content: body.content,
         important: body.important,
     }
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+         { content, important },
+         { new: true, runValidators: true, context: 'query' }
+         )
         .then(updatedNote => {
             response.json(updatedNote)
         })
