e1c860e3c63d4b5b703e2da946de87eeb8c77784
Michal Sostak <michal.sostak@gmail.com>
2023-07-11 10:57:42 +0200

    feat: backend testing

--- part4/backend/mongo.js
+++ part4/backend/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
@@ -8,9 +8,11 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://michalsostak:${password}@cluster0.f1vgnnl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  // `mongodb+srv://michalsostak:${password}@cluster0.f1vgnnl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://michalsostak:${password}@cluster0.f1vgnnl.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
@@ -19,6 +21,31 @@ const noteSchema = new mongoose.Schema({
 })
 
 const Note = mongoose.model('Note', noteSchema)
+
+
+
+// Post new notes
+
+// const note1 = new Note({
+//   content: 'HTML is Easy',
+//   important: true,
+// })
+
+// note1.save().then(() => {
+//   console.log('note1 saved!')
+//   mongoose.connection.close()
+// })
+
+// const note = new Note({
+//   content: 'Mongoose makes things easy',
+//   // date: new Date(),
+//   important: true,
+// })
+
+// note.save().then(() => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
 
 Note.find({ important: true }).then(result => {
   result.forEach(note => {
