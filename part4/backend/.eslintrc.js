5de4998b825bf7809332fdeaaf3b0f4e2429c6d1
Michal Sostak <michal.sostak@gmail.com>
2023-07-11 22:52:46 +0200

    feat: backend adding no unused var error ignore for deconstructed variables to eslintsrc

--- part4/backend/.eslintrc.js
+++ part4/backend/.eslintrc.js
@@ -47,6 +47,9 @@ module.exports = {
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
     ],
-    'no-console': 0
+    'no-console': 0,
+    'no-unused-vars': [
+      'error', { 'ignoreRestSiblings': true }
+    ]
   }
 }
