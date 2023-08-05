31439228a55d7d3e7ff5c80ba6a18eda98a83f66
Michal Sostak <michal.sostak@gmail.com>
2023-08-05 16:55:26 +0200

    feat: part 6c notes

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,23 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
-
-import filterReducer from './reducers/filterReducer'
-import noteService from './services/notes'
-import noteReducer, { setNotes } from './reducers/noteReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-noteService.getAll().then(notes =>
-  store.dispatch(setNotes(notes))
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
